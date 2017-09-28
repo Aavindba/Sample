515ec8d2c12a1aece5f0ea53831e4e882ceea145
Aavindba <arvindbcps@gmail.com>
2017-09-28 10:09:26 -0500

    Creating Reactjs

--- InstallReactjsNodejsandNpm.js
+++ InstallReactjsNodejsandNpm.js
@@ -2,7 +2,7 @@ Instructions to Install Reactjs., Nodejs and Npm
 To work on Reactjs We need to install environment nodejs and npm 
 Goto /* https://nodejs.org/en/ */ and select Appropriate version that is compatible with your Device 
 Double click the downloaded file and proceed with Instructions
-One you are done with installation 
+Once you are done with installation 
 Then open command prompt line on system or Terminal from IDE (i worked on windows)
 Follow the commands
 Node -v 
@@ -10,5 +10,16 @@ It will display version if installaion went good
 npm -v 
 Gives version installed on system 
 
-Once you are good with this Start installaion of npm library 
+Once you are good with this Start installaion of reactjs using CommandPromptline
+
+npm install -g create-react-app
+create-react-app my-app (my-app is the name of the file)
+
+cd my-app 
+npm start (This will automatically install)
+
+If this is Sucessful  
+You can now view my-app in the browser.
+
+  Local:            http://localhost:3000/
 
