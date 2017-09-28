e255479cfa77a00ebeab0c268e44f2939b771884
Aavindba <arvindbcps@gmail.com>
2017-09-28 10:02:14 -0500

    Test nodejs and Npm versions

--- InstallReactjsNodejsandNpm.js
+++ InstallReactjsNodejsandNpm.js
@@ -1,6 +1,14 @@
 Instructions to Install Reactjs., Nodejs and Npm
-To work on Reactjs We need to install environment nodejs and Library npm 
+To work on Reactjs We need to install environment nodejs and npm 
 Goto /* https://nodejs.org/en/ */ and select Appropriate version that is compatible with your Device 
-Then open command prompt or Terminal from IDE 
+Double click the downloaded file and proceed with Instructions
+One you are done with installation 
+Then open command prompt line on system or Terminal from IDE (i worked on windows)
 Follow the commands
+Node -v 
+It will display version if installaion went good 
+npm -v 
+Gives version installed on system 
+
+Once you are good with this Start installaion of npm library 
 
