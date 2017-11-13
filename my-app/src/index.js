02bff353efc9c494138001c64cee685abf6cb6db
Aavindba <arvindbcps@gmail.com>
2017-11-12 21:36:46 -0600

    modified for Tick Function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// function San()
+// {
 ReactDOM.render(<App />, document.getElementById('root'));
+// }
+// setInterval(San, 1000);
 registerServiceWorker();
